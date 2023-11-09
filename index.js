76e6f99e699fae2911602362278bc7dfac18fb88
omaneh <122321976+omaneh@users.noreply.github.com>
2023-11-08 19:58:20 -0500

    Added MongoDb -- need help renaming collection

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const requestURL = require("./services/service.js");
-
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 const app = express();
+const connectDB = require('./database/database');
+const Stations = require('./database/models/station');
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 
-app.get("/", (req, res) => {
+// this should do the same thing as the code above. 
+app.use(cors()); 
+
+
+
+app.get("/arrivals", (req, res) => {
   /* Console Log Test */
   console.log("Just got pinged!!!");
 
@@ -18,3 +26,16 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+connectDB();
+// mongo DB api
+
+app.get('/data/api/get/stations', (req, res) => {
+  Stations.find({})
+    .then((result) => {
+      res.send(result)
+    })
+      .catch((err) => {
+        console.log(err)
+      });
+})
