ce2bf9898b9e1a40dbc211a1737002cb764cb128
transitatl <151540667+transitatl@users.noreply.github.com>
2023-11-20 18:18:59 -0500

    Update database.js

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const CONNECTION_STRING = process.env.DB_STRING;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(CONNECTION_STRING, {
+    const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
