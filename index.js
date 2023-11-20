ea87c5a61f75d24277d32a644c5f4553f5dcb7d7
transitatl <151540667+transitatl@users.noreply.github.com>
2023-11-20 18:37:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.get("/arrivals", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on ${PORT} def`));
-connectDB().then(() => {
-})
 
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`listening on ${PORT}`));
+})
 // mongo DB api
 
 app.get('/api/get/all/stations', (req, res) => {
