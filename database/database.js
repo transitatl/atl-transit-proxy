59a7859248023ab4324e4dbf0d6833a41ccde8be
transitatl <151540667+transitatl@users.noreply.github.com>
2023-11-20 18:29:14 -0500

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const CONNECTION_STRING = process.env.DB_STRING;
+//const CONNECTION_STRING = process.env.DB_STRING;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
+    const conn = await mongoose.connect("mongodb+srv://transitatl:Rg3o8MkHdu7MZY8W@cluster0.nkmqyiv.mongodb.net/TransitAtlTrain?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
